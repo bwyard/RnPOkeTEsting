a05be6fcbbbcc5e4dc4dec6cbd5eab9c7a0a7ee6
bwyard <bwyard@gmail.com>
2020-12-10 09:05:06 -0600

    added captured list

---
+++ src/components/CapturedPokemons.js
@@ -0,0 +1,19 @@
+import React, { useContext, useState } from "react";
+import { PokemonContext } from "../../PokemonContext";
+
+const CapturedPokemons = () => {
+    const { capturedPokemons, release } = useContext(PokemonContext);
+    return (
+        <div className='pokedex'>
+            <h2>Captured Pokemon</h2>
+
+            {capturedPokemons.map((pokemon) =>
+            <div key = {`${pokemon.id}-${pokemon.name}`}>
+                <span>{pokemon.name}</span>
+                <button onClick={release(pokemon)}>-</button>
+            </div>)}
+        </div>
+    )
+}
+
+export default CapturedPokemons
