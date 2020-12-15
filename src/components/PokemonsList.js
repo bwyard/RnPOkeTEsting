8517133dd2d93774ae2a4e38c55837c7ef6043ad
bwyard <bwyard@gmail.com>
2020-12-15 11:05:29 -0600

    added useeffect hook

--- src/components/PokemonsList.js
+++ src/components/PokemonsList.js
@@ -1,22 +1,35 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { PokemonContext } from "../../PokemonContext";
+
+const url = 'https://pokeapi.co/api/v2/pokemon?limit=25'
 
 const PokemonsList = () => {
     const {
-        pokemons,
-        capture
+        state,
+        capture,
+        addPokemons
     } = useContext(PokemonContext)
+
+    useEffect(() => {
+        const fetchPokemons = async () => {
+          const response = await fetch(url);
+          const data = await response.json();
+          addPokemons(data.results); // update the pokemons list with this data
+        };
+      
+        fetchPokemons();
+      }, []);
     
 
     return(
         <div className="pokemons-list">
             <h2>Pokemons List</h2>
 
-            {pokemons.map((pokemon) =>
+            {state.pokemons.sort((a,b)=>(a.name.toLowerCase()>b.name.toLowerCase()) ?1:-1).map((pokemon) =>
             <div key={`${pokemon.id}-${pokemon.name}`}>
                 <div>
-                    <span>{pokemon.name}</span>
-                    <button onClick={capture(pokemon)}>+</button>
+                    <span>{pokemon.name.toUpperCase().substr(0,1).concat(pokemon.name.substr(1))}</span>
+                    <button onClick={capture(pokemon)}>Capture</button>
                 </div>
             </div>)}
         </div>
